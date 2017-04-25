d895b00bf2a43852f85f96e8e356dea445e0d5e5
Alejandro Ochoa <alex.jym13@gmail.com>
2017-04-24 20:26:11 -0500

    se agregaron las activities que se necesitaran y se creo la vista de el lector QR

--- app/src/main/java/arenzo/alejandroochoa/osopolar/MainActivity.java
+++ app/src/main/java/arenzo/alejandroochoa/osopolar/MainActivity.java
@@ -1,24 +1,44 @@
 package arenzo.alejandroochoa.osopolar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnNuevaVenta;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        centerTitle();
+        centrarTituloActionBar();
+        cargarElementosVista();
+        btnNuevaVenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarEscaneo();
+            }
+        });
     }
 
-    private void centerTitle() {
+    private void cargarElementosVista(){
+        btnNuevaVenta = (Button)findViewById(R.id.btnNuevaVenta);
+    }
+
+    private void mostrarEscaneo(){
+        Intent intent = new Intent(this, escaneo.class);
+        startActivity(intent);
+    }
+
+    private void centrarTituloActionBar() {
         ArrayList<View> textViews = new ArrayList<>();
 
         getWindow().getDecorView().findViewsWithText(textViews, getTitle(), View.FIND_VIEWS_WITH_TEXT);
