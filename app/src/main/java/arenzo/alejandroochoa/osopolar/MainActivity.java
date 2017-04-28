bd70472464f02622bf081d6c725003215e5d8ebc
Alejandro Ochoa <alex.jym13@gmail.com>
2017-04-27 19:53:08 -0500

    Se agrego el identificar el dispositivo con el alert dialog

--- app/src/main/java/arenzo/alejandroochoa/osopolar/MainActivity.java
+++ app/src/main/java/arenzo/alejandroochoa/osopolar/MainActivity.java
@@ -1,17 +1,32 @@
 package arenzo.alejandroochoa.osopolar;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TAG = "MainActivity";
+    private final String EXISTEIDEQUIPO = "EXISTEIDEQUIPO";
+    private final String IDEQUIPO = "IDEQUIPO";
+
+    private SharedPreferences sharedPreferences;
 
     private Button btnNuevaVenta;
     // PARA AGREGAR LA FUENTE POR CODIGO tv.setTypeface(Fuentes.myFont(this));
@@ -22,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         centrarTituloActionBar();
         cargarElementosVista();
+        agregarIdEquipo();
         btnNuevaVenta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void cargarElementosVista(){
         btnNuevaVenta = (Button)findViewById(R.id.btnNuevaVenta);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
     }
 
     private void mostrarEscaneo(){
@@ -65,4 +82,46 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+    private void agregarIdEquipo(){
+        Boolean saberSiExiste = sharedPreferences.getBoolean(EXISTEIDEQUIPO, false);
+        if (!saberSiExiste){
+            alertaAgregarIdEquipo();
+        }
+    }
+
+    private void alertaAgregarIdEquipo(){
+        LayoutInflater inflater = (LayoutInflater)getApplicationContext().getSystemService
+                (getApplicationContext().LAYOUT_INFLATER_SERVICE);
+        final View view = inflater.inflate(R.layout.dialog_inicio_id, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("ID Equipo")
+                .setMessage("Agrega un ID para iniciar sesión")
+                .setView(view)
+                .setPositiveButton("INICIAR", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setCancelable(false);
+        final AlertDialog dialog = builder.create();
+        dialog.show();
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText edtID = (EditText)view.findViewById(R.id.edtIdEquipo);
+                if (edtID.length() == 4){
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean(EXISTEIDEQUIPO, true);
+                    editor.putInt(IDEQUIPO, Integer.parseInt(edtID.getText().toString()));
+                    editor.commit();
+                    dialog.dismiss();
+                }else{
+                    Toast.makeText(MainActivity.this, "Debes de añadir un ID de cuatro digitos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
 }
