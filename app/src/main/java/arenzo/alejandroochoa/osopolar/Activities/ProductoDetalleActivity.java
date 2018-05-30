05fd4312a582d64d07a1cb88df7feac2b61588c6
Luis Cobian <lcobian_@ucol.mx>
2018-05-30 13:17:17 -0500

    activdad de productodetalle del detalle venta

---
+++ app/src/main/java/arenzo/alejandroochoa/osopolar/Activities/ProductoDetalleActivity.java
@@ -0,0 +1,15 @@
+package arenzo.alejandroochoa.osopolar.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import arenzo.alejandroochoa.osopolar.R;
+
+public class ProductoDetalleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_producto_detalle);
+    }
+}
