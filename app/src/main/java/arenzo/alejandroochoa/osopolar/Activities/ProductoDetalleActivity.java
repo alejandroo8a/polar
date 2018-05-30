1208b2f277a00e2b60becd157378514a6bc08e84
Luis Cobian <lcobian_@ucol.mx>
2018-05-30 13:25:30 -0500

    listview of productoDetals in layout

--- app/src/main/java/arenzo/alejandroochoa/osopolar/Activities/ProductoDetalleActivity.java
+++ app/src/main/java/arenzo/alejandroochoa/osopolar/Activities/ProductoDetalleActivity.java
@@ -2,11 +2,17 @@ package arenzo.alejandroochoa.osopolar.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.List;
 
 import arenzo.alejandroochoa.osopolar.R;
+import arenzo.alejandroochoa.osopolar.SQlite.baseDatos;
 
 public class ProductoDetalleActivity extends AppCompatActivity {
-
+    private baseDatos bd;
+    List<String> names;
+    ListView listhistorial;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
